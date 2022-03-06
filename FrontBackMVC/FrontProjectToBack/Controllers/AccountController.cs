2c5652ad12228a251c30e4716b530ee08e9f1121
rovshanguli <rovshanshg@code.edu.az>
2022-03-06 16:02:09 +0400

    FrontBack

---
+++ FrontBackMVC/FrontProjectToBack/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontProjectToBack.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
